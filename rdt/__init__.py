8649eeb65bab989009d50ba5f7af827f79805dd5
oiwn <alex@imscraping.ninja>
2021-07-16 13:49:12 +0700

    RedisUniqueQueue to the toplevel

--- rdt/__init__.py
+++ rdt/__init__.py
@@ -1,4 +1,5 @@
 """Top level imports"""
 from .queues import RedisLifoQueue
+from .unique_queue import RedisUniqueQueue
 from .filters import RedisSetFilter, RedisBucketFilter
 from .counters import RedisCounters
